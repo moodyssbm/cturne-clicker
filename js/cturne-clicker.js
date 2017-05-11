f0c713c690faac49212c070ccb4c662a160baf0a
moodyssbm <moodyssbm@gmail.com>
2017-05-11 08:22:12 -0400

    big update. game is playable now

--- js/cturne-clicker.js
+++ js/cturne-clicker.js
@@ -27,11 +27,18 @@ function updateMacca() {
 let Demon = function(name, price, mps) {
   this.name = '<h4>';
   this.name += name;
-  this.name += ' <input type="button" value="Buy" onclick="buy(pixie)">';
+  this.name += ' <input type="button" value="Buy" onclick="buy(';
+  this.name += name.toLowerCase();
+  this.name += ')">';
   this.name += '</h4>';
   this.price = price;
   this.mps = mps/60;
   this.owned = 0;
+  this.stats = 'Price: ' + this.price.toFixed(2) + ' Macca // Owned: ' + this.owned + ' // MpS: ' + (this.mps * 60 * this.owned).toFixed(2);
+}
+
+function getDemonStats(demon) {
+  return 'Price: ' + demon.price.toFixed(2) + ' Macca // Owned: ' + demon.owned + ' // MpS: ' + (demon.mps * 60 * demon.owned).toFixed(2);
 }
 
 // making new Demons
@@ -41,11 +48,38 @@ let party = [pixie];
 // Drawing the demons on screen
 for(i=0; i!=party.length; i++) {
   demonArea.innerHTML += party[i].name;
+  demonArea.innerHTML += party[i].stats;
 }
 
 // function for buying demons
 function buy(demon) {
-  if(demon == pixie && score >= pixie.price) {
-    console.log('You bought a pixie');
+  if(score >= demon.price) {
+    demon.owned++;
+    score -= demon.price;
+    demon.price = demon.price * unitGrowth;
+  }
+
+  updateDemonStats();
+}
+
+// Function that returns proper FPS value
+function fps(n) { return 1000/n; }
+
+// This will be called every time our demons' stats update:
+function updateDemonStats() {
+  demonArea.innerHTML = '<h3>Demons</h3>';
+  for(i=0; i!=party.length; i++) {
+    demonArea.innerHTML += party[i].name;
+    party[i].stats = getDemonStats(party[i]);
+    demonArea.innerHTML += party[i].stats;
   }
 }
+
+// Setting the main clock of the game
+mainClock = setInterval(function() {
+  for(i=0; i!=party.length; i++) {
+    score += party[i].mps * party[i].owned;
+  }
+
+  updateMacca();
+},fps(60));
